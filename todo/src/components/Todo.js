8bc95724a241ebb7c2664a14844595f2361c90da
wahidtaamallah <wahidtaamallah92@hotmail.com>
2020-07-29 02:34:35 +0100

    new todolist

--- todo/src/components/Todo.js
+++ todo/src/components/Todo.js
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './Todo.css'
 
-function Todo ({message, id, deleteTodo}) { 
-   return (
-   <div className="Todo">
-       <p>{message} </p>
-    <div><button onClick={() => deleteTodo(id)}>Delete </button></div>
-    </div>
-  )}
- export default Todo
+
+function Todo({todo, removeTodo}) {
+  const [checkBoxValue, setCheckBoxValue] = useState(false)
+      
+    return (
+        <div className="todo_container">
+                <input type="checkbox" 
+                 onChange={() => setCheckBoxValue(!checkBoxValue)}
+                />
+               {
+                 checkBoxValue === true ?
+                 <p contentEditable="true"
+                 className="through"
+                >{todo.todoName}</p>
+                :
+                <p contentEditable="true"
+                >{todo.todoName}</p>
+               }
+              
+                            
+                <i className="fa fa-trash" 
+                   onClick= {() => removeTodo(todo.todoName)}
+                ></i>
+        </div>
+    )
+}
+
+
+export default Todo
--- todo/src/components/TodoList.js
+++ todo/src/components/TodoList.js
@@ -1,33 +1,18 @@
-import React, {useState} from 'react'
+import React from 'react';
 import Todo from './Todo'
-const handleSubmit = (e, todoList, setTodoList, input, setInput) => {
-    e.preventDefault()
-    const id = (todoList.lenght) ? todoList[todoList.lenght - 1].id + 1 : 0
 
-  setTodoList([...todoList, {id: id, message: input}])
-  setInput('')
-}
 
-const deleteTodo = (id, todoList, setTodoList) => {
-    setTodoList(todoList.filter(todo => todo.id !== id))
-}
+function TodoList({todos, removeTodo}) {
 
- export default  () => {
-     const [todoList, setTodoList] = useState([
-         {id: 1, message: ' '},
-     ])
-     const [input, setInput] = useState('  ')
- 
-    return (
-    <div className="TodoList">
-        <form onSubmit={(e) => handleSubmit(e, todoList, setTodoList, input, setInput)}>
-            <input onChange={(e) => setInput(e.target.value) } value={input}/>
-            <button type="submit">Add</button>
-        </form>
-        {todoList.map(todo => (
-            <Todo message={todo.message} id={todo.id} deleteTodo={(id) => deleteTodo(id, todoList, setTodoList)}/>
-        ))}        
-    </div>
-  )
-} 
-  
+    return(
+        todos.map((todo, index) => <Todo
+            key={index}
+            todo={todo}
+            removeTodo = {removeTodo}
+           /> )
+    )
+}   
+
+
+
+export default TodoList
